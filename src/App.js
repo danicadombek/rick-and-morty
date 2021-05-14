493bb054222f8e7f65e8b48d0e170b185536b298
Danica Dombek <danica.dombek@gmx.de>
2021-05-14 13:20:50 +0200

    Create header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
+import './Header'
+import Header from './Header'
 
 export default function App() {
-  const url = 'https://rickandmortyapi.com/api/character'
   const [characters, setCharacters] = useState([])
+  const url = 'https://rickandmortyapi.com/api/character'
 
   useEffect(() => {
     fetch(url)
       .then(res => res.json())
-      .then(resBody => setCharacters(resBody.results))
-  }, [url])
+      .then(res => setCharacters(res.results))
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     <div className="App">
+      <Header />
       <ul>
         {characters.map(el => (
           <li>{el.name}</li>
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <section className="Header">
+      <h1>Rick and Morty</h1>
+    </section>
+  )
+}
