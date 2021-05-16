282e1f24211a6f4a58d35cf5dcf77dae6a45864d
Danica Dombek <danica.dombek@gmx.de>
2021-05-16 22:40:15 +0200

    Refine design

--- src/Location.js
+++ src/Location.js
@@ -4,9 +4,8 @@ import './Location.css'
 export default function Location({ id, name, dimension }) {
   return (
     <section key={id} className="Location">
-      <h2>Location</h2>
+      <h2>{name}</h2>
       <ul className="Location-list">
-        <li>Name: {name}</li>
         <li>Dimension: {dimension}</li>
       </ul>
     </section>
